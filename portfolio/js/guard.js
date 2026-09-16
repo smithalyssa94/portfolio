b2f2b9023e7c9abf8e2b49cf167f01f739937506
smithalyssa94 <smithalyssa94@gmail.com>
2026-09-16 15:28:54 -0500

    Remove duplicate portfolio folder

--- portfolio/js/guard.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  try {
-    if (sessionStorage.getItem("gateUnlocked") !== "1") {
-      window.location.replace("../index.html");
-    }
-  } catch (e) {
-    window.location.replace("../index.html");
-  }
-})();
